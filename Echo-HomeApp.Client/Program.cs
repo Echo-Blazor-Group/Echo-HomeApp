07ce775fb90b754745b3574bbc00f5594820b254
Samsal <43518070+baclava1001@users.noreply.github.com>
2024-05-03 10:11:43 +0200

    + Realtorfirm CR(U)D mostly working, except some rerouting trouble. + Added Bootstrap for Blazor nuget. + Added som global usings in _Imports

--- Echo-HomeApp.Client/Program.cs
+++ Echo-HomeApp.Client/Program.cs
@@ -8,4 +8,7 @@ builder.Services.AddScoped(sp => new HttpClient
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 });
 
+// TODO: (Samed) remove this, anad also from nuget packs and imports - if decided not to use
+builder.Services.AddBlazorBootstrap();
+
 await builder.Build().RunAsync();
--- Entities/DTOs/UpdateEstateDto.cs
+++ Entities/DTOs/UpdateEstateDto.cs
@@ -1,4 +1,4 @@
-﻿using Entities;
+﻿using Models;
 
 namespace DTOs
 {
