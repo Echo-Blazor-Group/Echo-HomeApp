591ced55e69292b4296dd4f5afd43125bbce774d
Samsal <43518070+baclava1001@users.noreply.github.com>
2024-05-06 08:57:49 +0200

    Working on RealtorFirm.

--- Echo-HomeApp.Client/Program.cs
+++ Echo-HomeApp.Client/Program.cs
@@ -3,12 +3,19 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-builder.Services.AddScoped(sp => new HttpClient
+// Author: Samed
+// TODO: Ta bort om ej nödvändigt
+HttpClientHandler handler = new HttpClientHandler()
+{
+    AllowAutoRedirect = false
+};
+
+builder.Services.AddScoped(sp => new HttpClient(handler)
 {
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 });
 
-// TODO: (Samed) remove this, anad also from nuget packs and imports - if decided not to use
+// TODO: (Samed) remove this, and also from nuget packs and imports - if decided not to use
 builder.Services.AddBlazorBootstrap();
 
 await builder.Build().RunAsync();
