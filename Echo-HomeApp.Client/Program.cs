0055cbfd5653f2f0e34d1d67d95c0a03f40efef3
Samsal <43518070+baclava1001@users.noreply.github.com>
2024-05-07 16:42:46 +0200

    RealtorFirm CRUD functional and ready.

--- Echo-HomeApp.Client/Program.cs
+++ Echo-HomeApp.Client/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 // Author: Samed
-// TODO: Ta bort om ej nödvändigt
+// TODO: (Samed) Ta bort om ej nödvändigt
 HttpClientHandler handler = new HttpClientHandler()
 {
     AllowAutoRedirect = false
@@ -15,7 +15,6 @@ builder.Services.AddScoped(sp => new HttpClient(handler)
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 });
 
-// TODO: (Samed) remove this, and also from nuget packs and imports - if decided not to use
 builder.Services.AddBlazorBootstrap();
 
 await builder.Build().RunAsync();
