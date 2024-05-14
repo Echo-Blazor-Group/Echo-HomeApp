271aa2881f5579ea318db2e93aa2661dd37b0c43
gustaf frejd <gustaf.frejd@hotmail.com>
2024-05-14 16:24:00 +0200

    alot

--- Echo-HomeApp.Client/DTOs/DeleteEstateDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Echo_HemAPI.Data.Models.DTOs.EstateDtos
-{
-    public class DeleteEstateDto
-    {
-        public int Id { get; set; }
-        public bool OnTheMarket { get; set; } = true;
-
-    }
-}
