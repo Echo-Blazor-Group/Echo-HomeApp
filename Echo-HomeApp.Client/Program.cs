c1b4a02fd8fe0912e74d79a4ccd1ca807089634f
Samsal <43518070+baclava1001@users.noreply.github.com>
2024-05-14 12:40:49 +0200

    Login and authentication against API working.

--- Echo-HomeApp.Client/Program.cs
+++ Echo-HomeApp.Client/Program.cs
@@ -9,14 +9,27 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 // Author: Samed
 
-// Add IHttpClientFactory for use in singleton services
-builder.Services.AddHttpClient().AddTransient<HttpMessageHandler, AuthenticationHandler>();
+
 // AuthenticationHandler passes JWT back to API along with HttpClient-requests
 builder.Services.AddTransient<AuthenticationHandler>();
-builder.Services.AddTransient(sp => new HttpClient()
+
+// Add IHttpClientFactory for use in singleton services
+builder.Services.AddHttpClient("ServerApi").
+    ConfigureHttpClient(c => c.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)).AddHttpMessageHandler<AuthenticationHandler>();
+
+// Configure HttpClient that is used for injection in components
+builder.Services.AddTransient(sp =>
 {
-    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-}).AddTransient<HttpMessageHandler, AuthenticationHandler>();
+    var handler = new HttpClientHandler();
+    var authHandler = sp.GetService<AuthenticationHandler>();
+    authHandler.InnerHandler = handler;
+
+    return new HttpClient(authHandler)
+    {
+        BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+    };
+});
+
 
 // Services for authentication as singletons for longer lifespan
 builder.Services.AddSingleton<IAuthenticationService, AuthenticationService>();
