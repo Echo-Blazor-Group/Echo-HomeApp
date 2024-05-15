62406958ac66667e837e345776b48f6edf0898d6
gustaf frejd <gustaf.frejd@hotmail.com>
2024-05-15 15:20:58 +0200

    Fixed filtering. Added a Carousel feature though hard to actually use

--- Entities/DTOs/EstateDtos/DeleteEstateDto.cs
+++ Entities/DTOs/EstateDtos/DeleteEstateDto.cs
@@ -1,9 +1,10 @@
-﻿namespace Echo_HemAPI.Data.Models.DTOs.EstateDtos
+﻿namespace DTOs.EstateDtos
 {
     public class DeleteEstateDto
     {
         public int Id { get; set; }
         public bool OnTheMarket { get; set; } = true;
+
 
     }
 }
